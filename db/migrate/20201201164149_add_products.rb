9618bc5ec2087e98b24d16e635adc90e49fef95f
Alejaldo <statfluct@yahoo.com>
2020-12-01 22:46:09 +0500

    Add products

---
+++ db/migrate/20201201164149_add_products.rb
@@ -0,0 +1,9 @@
+class AddProducts < ActiveRecord::Migration[6.0]
+  def change
+    Product.create :title => 'Margarita', :description => 'This is classic italian pizza', :price => 9, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/margarita.jpg'
+
+    Product.create :title => 'Pepperoni', :description => 'This is old spicy italian pizza', :price => 11, :size => 30, :is_spicy => true, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/pepperoni.jpg'
+
+    Product.create :title => 'Vegetarian', :description => 'This is vegan pizza', :price => 7, :size => 30, :is_spicy => false, :is_veg => true, :is_best_offer => true, :path_to_image => '/images/vegan.jpg'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_063848) do
+ActiveRecord::Schema.define(version: 2020_12_01_164149) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
