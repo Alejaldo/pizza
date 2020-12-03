5303d58c476fee0a39455632344e1babc79126e4
Alejaldo <statfluct@yahoo.com>
2020-12-03 12:06:50 +0500

    Add total_products() function

--- public/app.js
+++ public/app.js
@@ -17,3 +17,21 @@ function add_to_cart(id)
         x = x*1 + 1;
         window.localStorage.setItem(key, x);
 }
+
+function total_products()
+{
+        var total = 0;
+
+        for(var i = 0; i < window.localStorage.length; i++)
+        {
+                var x = window.localStorage.key(i);
+                var y = window.localStorage.getItem(x);
+
+                if(x.indexOf('product_') == 0)
+                {
+                        total += y*1;
+                }
+        }
+
+        return total;
+}
